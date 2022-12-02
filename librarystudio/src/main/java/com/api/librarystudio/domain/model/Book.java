e43c600bcd586134a8ce8e97ce4131691045c839
Bruna Cristina Andrade <38590890+buandradels@users.noreply.github.com>
2022-12-02 20:27:50 -0300

    Add files via upload

---
+++ librarystudio/src/main/java/com/api/librarystudio/domain/model/Book.java
@@ -0,0 +1,77 @@
+package com.api.librarystudio.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private String synopsis;
+
+
+    @ManyToOne
+    @JoinColumn(name= "id_user")
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
